834f48845e5a57b5457da80e1dd7ba2729d6fb11
NanoNerd99 <mh.karimi350@gmail.com>
2024-05-16 01:16:22 -0400

    change vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/metric/",
+  base: "https://nanonerd99.github.io/metric/",
 })
